efd4e6f9d813aae01f25879a505c6c010f27746e
MistiDev <vladikzvezda1997@gmail.com>
2024-07-24 02:05:31 +0300

    add

--- src/index.js
+++ src/index.js
@@ -56,7 +56,7 @@ class MiBase {
         if(!fs.existsSync(`${this.path}/${name}.db`)) {
             const jsonData = typeof data === 'object' ? data : JSON.parse(data);
             const stringData = JSON.stringify(jsonData);
-            fs.writeFileSync(filePath, stringData);
+            fs.writeFileSync(`${this.path}/${name}.db`, stringData);
             this.tables = fs.readdirSync(this.path);
         } else {
             console.log(`The table ${name} already exists!`);
